9648df2be6fa1609826eb2e80eb17ae2f6b09748
Pradeep-M77 <pradeepmanoher1210@gmail.com>
2024-09-27 14:13:29 +0530

    Work done

--- VastEcommerce/src/main/java/com/vast/ser/Handle.java
+++
@@ -1,8 +0,0 @@
-package com.vast.ser;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-public interface Handle {
-   String execute(HttpServletRequest request,HttpServletResponse response);
-}
